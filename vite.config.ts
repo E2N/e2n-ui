1d8aea2ee204334be9d63ec0dddce4e94b1ea98f
Sonja Feitsch <feitsch@e2n.de>
2024-03-01 14:43:16 +0100

    update version to 0.0.5

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/index.ts"),
+
+      name: "e2n-ui",
+      // the proper extensions will be added
+      fileName: "e2n-ui",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+  plugins: [dts(), react()],
+});
