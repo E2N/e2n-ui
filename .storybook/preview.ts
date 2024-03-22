104a35eb41964284fac5d16afac45cd8818b8d98
Kevin Kaufmann <kaufmann@e2n.de>
2024-03-22 15:06:36 +0100

    add storybook darkmode Support

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -10,6 +10,15 @@ const preview: Preview = {
       },
     },
   },
+  globalTypes: {
+    darkMode: {
+      defaultValue: true, // Enable dark mode by default on all stories
+    },
+    // Optional (Default: 'dark')
+    className: {
+      defaultValue: 'dark', // Set your custom dark mode class name
+    },
+  },
 };
 
 export default preview;
