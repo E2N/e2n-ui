6b07ecb4abd6035bc0be59d3fe89d4239112b578
Alfred Gramelt <gramelt@e2n.de>
2024-04-02 13:35:00 +0200

    size example

--- src/components/Carousel/Carousel.stories.tsx
+++ src/components/Carousel/Carousel.stories.tsx
@@ -56,6 +56,41 @@ export const Default = {
     </Carousel>
   ),
 };
+
+export function Size() {
+  return (
+    <Carousel
+      opts={{
+        align: "start",
+      }}
+      className="w-full max-w-sm mx-10">
+      <CarouselContent>
+        {Array.from({ length: 3 }).map((_, index) => (
+          <>
+            <CarouselItem
+              key={index + 1}
+              className="md:basis-1/2 lg:basis-1/3 pl-2 md:pl-4">
+              <Card header="Das ist eine Überschrift" content={<div>1</div>} />
+            </CarouselItem>
+            <CarouselItem
+              key={index + 2}
+              className="md:basis-1/2 lg:basis-1/3 pl-2 md:pl-4">
+              <Card header="Das ist eine Überschrift" content={<div>2</div>} />
+            </CarouselItem>
+            <CarouselItem
+              key={index + 3}
+              className="md:basis-1/2 lg:basis-1/3 pl-2 md:pl-4">
+              <Card header="Das ist eine Überschrift" content={<div>3</div>} />
+            </CarouselItem>
+          </>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
+
 export function Orientation() {
   return (
     <Carousel
