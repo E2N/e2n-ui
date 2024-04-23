68897f420da75867417a0d036bd0d956b38cb9ba
Sonja Feitsch <feitsch@e2n.de>
2024-04-23 07:47:23 +0200

    make switch work

--- src/components/Switch/Switch.stories.tsx
+++ src/components/Switch/Switch.stories.tsx
@@ -6,20 +6,14 @@ import { expect } from "@storybook/test";
 export default {
   title: "Components/Switch",
   component: Switch,
-  parameters: {
-    design: {
-      type: "figma",
-      url: "https://www.figma.com/file/dytYKVyXYobjZXq0BDPFsK/e2n-Web.16.05.23?type=design&node-id=1054%3A90286&t=vpySEPHRmHsccoM2-1",
-    },
-  },
 };
 
 export const Default: StoryObj<typeof Switch> = {
-  render: (args) => <Switch />,
-  // args: {
-  //   disabled: false,
-  //   onCheckedChange: () => console.log("changed"),
-  // },
+  render: (args) => <Switch {...args} />,
+  args: {
+    disabled: false,
+    onCheckedChange: () => console.log("changed"),
+  },
   play: async ({ canvasElement, step }) => {
     const canvas = within(canvasElement);
     await step("Toggle between states", async () => {
