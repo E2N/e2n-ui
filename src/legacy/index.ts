6e00e6f316ad88acc84192f422a49f109e6d9d2c
Sonja Feitsch <feitsch@e2n.de>
2024-08-20 08:12:58 +0200

    Fix exports für Tab und Sheet Komponenten (#21)
    * fix exports
    
    * fixExports
    
    ---------
    
    Co-authored-by: Alfred Gramelt <gramelt@e2n.de>

--- src/legacy/index.ts
+++ src/legacy/index.ts
@@ -2,5 +2,5 @@ export * from './Avatar';
 export * from './Button';
 export * from './Checkbox';
 export * from './Switch';
-export { LegacyTable as LegacyTable } from './Table';
+export * from './Table';
 export * from './Tab';
